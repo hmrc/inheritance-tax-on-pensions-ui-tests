76d71278d86d769a9257b9583fc0dcbaaccbd172
Ibi Awesu <26194202+way2autotesting@users.noreply.github.com>
2026-01-29 11:29:30 +0000

    Renamed the shell script to run-tests.sh to match the Jenkins pipeline configuration. v2

--- run-tests.sh
+++ run-tests.sh
@@ -1,6 +1,19 @@
+##!/usr/bin/env bash
+#
+#BROWSER=$1
+#ENVIRONMENT=$2
+#
+#sbt clean -Dbrowser="${BROWSER:=chrome}" -Denvironment="${ENVIRONMENT:=local}" test testReport
+
 #!/usr/bin/env bash
 
-BROWSER=$1
-ENVIRONMENT=$2
+DEFAULT_BROWSER=chrome
+BROWSER_TYPE=$1
+ENV=$2
 
-sbt clean -Dbrowser="${BROWSER:=chrome}" -Denvironment="${ENVIRONMENT:=local}" test testReport
+if [ -z "$BROWSER_TYPE" ]; then
+    echo "BROWSER_TYPE value not set, defaulting to $DEFAULT_BROWSER..."
+    echo ""
+fi
+
+sbt clean -Dbrowser="${BROWSER_TYPE:=$DEFAULT_BROWSER}" -Denvironment="${ENV:=local}" "testOnly uk.gov.hmrc.ui.ihtp.specs.*" test testReport
