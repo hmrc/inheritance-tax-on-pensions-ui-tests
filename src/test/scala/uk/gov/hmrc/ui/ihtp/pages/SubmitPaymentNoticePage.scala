d25d30670a5096e8e1819efbd4894499f491ed4b
Divya-Dhall <189638788+Divya-Dhall@users.noreply.github.com>
2026-07-01 01:14:24 +0100

    Submit Notice Pay

---
+++ src/test/scala/uk/gov/hmrc/ui/ihtp/pages/SubmitPaymentNoticePage.scala
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.ui.ihtp.pages
+
+import org.openqa.selenium.By
+
+object SubmitPaymentNoticePage extends BasePage {
+  override val pageUrl: String   = s"$baseUrl/pr-submit-payment-notice"
+  override val pageTitle: String =
+    "Did the PR submit the payment notice? - Report Inheritance Tax on a pension - GOV.UK"
+  
+
+  def clickRadioButton(text: String): Unit =
+    text match {
+      case "Yes"   => click(By.id("value"))
+      case "No" => click(By.id("value-no"))
+    }
+    clickSaveAndContinueButton()
+
+}
