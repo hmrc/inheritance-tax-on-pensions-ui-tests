9448299a2405eea38ca8b6cf12661e701c4479a0
Divya-Dhall <189638788+Divya-Dhall@users.noreply.github.com>
2026-09-10 14:04:39 +0100

    Trust updated for org

--- src/test/scala/uk/gov/hmrc/ui/ihtp/pages/NewSelectTypeOfBeneficiaryToAdd.scala
+++ src/test/scala/uk/gov/hmrc/ui/ihtp/pages/NewSelectTypeOfBeneficiaryToAdd.scala
@@ -27,7 +27,7 @@ object NewSelectTypeOfBeneficiaryToAdd extends BasePage {
   def clickRadioButton(text: String): Unit =
     text match {
       case "An individual"            => click(By.id("value_0"))
-      case "An organisation or trust" => click(By.id("value_1"))
+      case "A trust" => click(By.id("value_1"))
     }
     clickSaveAndContinueButton()
 
